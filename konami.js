99e01c8853f8e52d2ab9d0062cfa9d0bac94583d
Jordad <71786985+Jordad@users.noreply.github.com>
2021-10-30 18:33:26 -0500

    Update konami.js

--- konami.js
+++ konami.js
@@ -38,7 +38,7 @@ document.addEventListener('keydown', function(e) {
 });
 
 function activateCheats() {
-  document.body.style.backgroundImage = "url('https://i.imgur.com/y00QBzv.gif')";
+  document.body.style.backgroundImage = "url('https://i.imgur.com/MNfzmvS.gif')";
 
   var audio = new Audio('brainpower.mp3');
   audio.play();
